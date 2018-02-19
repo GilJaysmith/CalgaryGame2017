6a3b8ee6d5a21fde29c2d935ddd023d2675235cf
GilJaysmith <31742649+GilJaysmith@users.noreply.github.com>
2018-02-19 12:40:55 -0800

    Fix controls, test gamepad, subdivide physics timestep, pre-step game state before game loop to absorb any on-enter costs.

--- Engine/Vehicles/VehicleInputHandler.cpp
+++ Engine/Vehicles/VehicleInputHandler.cpp
@@ -24,17 +24,17 @@ void VehicleInputHandler::Update()
 {
 	// Keyboard.
 	m_Acceleration = Input::IsPressed(GLFW_KEY_I) ? 1.0f : 0.0f;
-	m_Brake = Input::IsPressed(GLFW_KEY_K) ? -1.0f : 0.0f;
+	m_Brake = Input::IsPressed(GLFW_KEY_K) ? 1.0f : 0.0f;
 	m_Handbrake = Input::IsPressed(GLFW_KEY_M);
-	m_Steer = Input::IsPressed(GLFW_KEY_J) ? -1.0f : Input::IsPressed(GLFW_KEY_L) ? 1.0f : 0.0f;
+	m_Steer = Input::IsPressed(GLFW_KEY_J) ? 1.0f : Input::IsPressed(GLFW_KEY_L) ? -1.0f : 0.0f;
 
 	// Gamepad.
 	GamepadHandler* gamepad = GamepadManager::GamepadForPlayer(m_PlayerIdx);
 	if (gamepad)
 	{
-		m_Acceleration = gamepad->Axis(GamepadAxis::RIGHT_TRIGGER);
-		m_Brake = gamepad->Axis(GamepadAxis::LEFT_TRIGGER);
+		m_Acceleration = (gamepad->Axis(GamepadAxis::RIGHT_TRIGGER) + 1.0f) / 2.0f;
+		m_Brake = (gamepad->Axis(GamepadAxis::LEFT_TRIGGER) + 1.0f) / 2.0f;
 		m_Handbrake = gamepad->ButtonPressed(GamepadButton::X);
-		m_Steer = gamepad->Axis(GamepadAxis::LEFT_STICK_HORIZ);
+		m_Steer = -gamepad->Axis(GamepadAxis::LEFT_STICK_HORIZ);
 	}
 }
